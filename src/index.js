306d09399cf59ff1e9db1c83e6169f1b3b5efd32
Mauro Junior <mauropereirajr@gmail.com>
2017-07-24 18:11:25 -0300

    feat: added `open` and `close` methods

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
-export default function hello() {
-  return 'Hello World.';
+import util from 'util';
+import { AbstractLevelDOWN } from 'abstract-leveldown';
+import { Database } from 'arangojs';
+
+
+exports.default = ArangoDOWN;
+
+
+function ArangoDOWN(arangoUri) {
+  AbstractLevelDOWN.call(this, arangoUri);
 }
+
+util.inherits(ArangoDOWN, AbstractLevelDOWN);
+
+
+ArangoDOWN.prototype._dbExists = function _dbExists({ createIfMissing, errorIfExists }, done) {
+  const dbName = this.location;
+
+  this._db.listDatabases((err, names) => {
+    if (err) {
+      return done(err);
+    }
+
+    const dbExists = names.includes(dbName);
+
+    if (!createIfMissing && !dbExists) {
+      return done(new Error(`Database ${dbName} does not exist`));
+    }
+
+    if (errorIfExists && dbExists) {
+      return done(new Error(`Database ${dbName} already exists`));
+    }
+
+    done();
+  });
+};
+
+
+ArangoDOWN.prototype._open = function _open(options, callback) {
+  const self = this;
+
+  self._db = new Database(self.location);
+
+  self.dbExists(options, callback);
+};
+
+ArangoDOWN.prototype._close = function _open(callback) {
+  process.nextTick(callback);
+};
